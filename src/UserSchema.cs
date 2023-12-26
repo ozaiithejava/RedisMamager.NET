98bb67227ccaf8d93b2fb14e918f31bdb3b2a62d
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-12-26 13:24:29 +0300

    Create UserSchema.cs

---
+++ src/UserSchema.cs
@@ -0,0 +1,8 @@
+public class UserSchema
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string LastLoginIP { get; set; }
+    public string Hwid { get; set; }
+}
